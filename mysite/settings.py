558a5a6664045e28eb41a644a37a6b490e84cef0
Anna <kukavskaia@bk.ru>
2017-10-07 16:18:23 +0300

    Updates allowed host

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2#$6!$7wh&zwm7036&5ge-5ldwyzcbtb%5xc!8w-z%8(&t06e%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'kukavskaia.pythonanywhere.com']
 
 
 # Application definition
